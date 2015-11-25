0bfd28d3192bcb4bfa64fdadc5cce679b6dbbcd7
Yoggi Firmanda <y.firmanda@netpoint-media.de>
2015-11-25 10:12:24 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<!-- Dropzone -->
+<link rel="stylesheet" type="text/css" href="dropzone/css/dropzone.css">
+<script src="dropzone/dropzone.js"></script>
+
+<body style="margin-left:50px;margin-top:50px;">
+
+<form action="api.php" class="dropzone">
+  <div class="fallback">
+    <input name="file" type="file" multiple />
+  </div>
+</form>
+</body>
+</html>
