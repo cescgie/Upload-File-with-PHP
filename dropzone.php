fc0c4a1251daa6a095a24b30a445276cd284d80a
Yoggi Firmanda <yoggifirmanda@gmail.com>
2016-01-06 11:04:34 +0100

    Update Dropzone

--- dropzone.php
+++ dropzone.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html>
-
-<!-- Dropzone -->
-<link rel="stylesheet" type="text/css" href="dropzone/css/dropzone.css">
-<script src="dropzone/dropzone.js"></script>
-
-<body style="margin-left:50px;margin-top:50px;">
-
-<form action="api.php" class="dropzone">
-  <div class="fallback">
-    <input name="file" type="file" multiple />
-  </div>
-</form>
-</body>
+  <header>
+    <title>Dropzone</title>
+    <!-- Dropzone -->
+    <link rel="stylesheet" type="text/css" href="dropzone/css/dropzone.css">
+    <script src="dropzone/dropzone.js"></script>
+  </header>
+  <body style="margin-left:50px;margin-top:50px;">
+    <h1>Multiple Uploads with Drag and Drop</h1>
+    <form action="api.php" class="dropzone">
+      <div class="fallback">
+        <input name="file" type="file" multiple />
+      </div>
+    </form>
+  </body>
 </html>
